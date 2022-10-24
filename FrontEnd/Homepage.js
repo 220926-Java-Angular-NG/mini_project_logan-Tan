2a40651fddedfb5236ca2921196fb169deef4987
Logan <logan.tan@wsu.edu>
2022-10-23 22:47:24 -0700

    Streach Goal added

--- FrontEnd/Homepage.js
+++ FrontEnd/Homepage.js
@@ -25,7 +25,7 @@ document.getElementById("hori").addEventListener('click',async(event)=>{
                 alert(`error Status: ${raw_response.status}`);
             }
             const json_data = await raw_response.json();
-            document.getElementById("horoscope").innerHTML = `Name: ${json_data.horoscope}`;
+            document.getElementById("horoscope").innerHTML = `Horoscope: ${json_data.horoscope}`;
             mood = json_data.meta.mood;
         }catch(error){
             console.log(error);
@@ -49,7 +49,7 @@ document.getElementById("hori").addEventListener('click',async(event)=>{
 });
 
 document.getElementById("getmood").addEventListener('click',async(event)=>{
-    if(mood){
+    if(!mood){
         try{
             const raw_response=await
             fetch('http://localhost:8080/getmood', {
@@ -62,11 +62,20 @@ document.getElementById("getmood").addEventListener('click',async(event)=>{
                 })
             });
             const json_data = await raw_response.json();
-            document.getElementById("mood").innerHTML = json_data.mood;
+            if(json_data.mood != undefined){
+                document.getElementById("mood").innerHTML = json_data.mood;
+            }else{
+                document.getElementById("mood").innerHTML = "No Mood has been Found yet";
+            }
         }catch(error){
             console.log(error);
         }
     } else{
         document.getElementById("mood").innerHTML = mood;
     }
+});
+
+document.getElementById("return").addEventListener('click',async(event)=>{
+    window.history.pushState(null,null,'http://127.0.0.1:5500/FrontEnd/Hori.html');
+    location.href = 'http://127.0.0.1:5500/FrontEnd/Hori.html';        
 });
--- FrontEnd/Register.js
+++ FrontEnd/Register.js
@@ -53,3 +53,95 @@ document.getElementById("return").addEventListener('click',async(event)=>{
     window.history.pushState(null,null,'http://127.0.0.1:5500/FrontEnd/Hori.html');
     location.href = 'http://127.0.0.1:5500/FrontEnd/Hori.html';        
 });
+let signd = document.getElementById("sign");
+function getsign(event){
+    var date = new Date(document.getElementById("birthday").value);
+    var month = date.getMonth()+1;
+    var day = date.getDate()+1;
+    switch(month){
+        case 1: // January
+            if(day <= 19){
+                signd.value = 'capricorn';
+            }else{
+                signd.value = 'aquarius'
+            }
+            break;
+        case 2:
+            if(day < 19){
+                signd.value = 'aquarius';
+            }else{
+                signd.value = 'pisces'
+            }
+            break;
+        case 3:
+            if(day < 21){
+                signd.value = 'pisces';
+            }else{
+                signd.value = 'aries';
+            }
+            break;
+        case 4:
+            if(day < 19){
+                signd.value = 'aries';
+            }else{
+                signd.value = 'taurus';
+            }
+            break;
+        case 5:
+            if(day < 21){
+                signd.value = 'taurus';
+            }else{
+                signd.value = 'gemini';
+            }
+            break;
+        case 6:
+            if(day < 21){
+                signd.value = 'gemini';
+            }else{
+                signd.value = 'cancer';
+            }
+            break;
+        case 7:
+            if(day < 23){
+                signd.value = 'cancer';
+            }else{
+                signd.value = 'leo';
+            }
+            break;
+        case 8:
+            if(day < 23){
+                signd.value = 'leo';
+            }else{
+                signd.value = 'virgo';
+            }
+            break;
+        case 9:
+            if(day < 23){
+                signd.value = 'virgo';
+            }else{
+                signd.value = 'libra';
+            }
+            break;
+        case 10:
+            if(day < 23){
+                signd.value = 'libra';
+            }else{
+                signd.value = 'scorpio';
+            }
+            break;
+        case 11:
+            if(day < 22){
+                signd.value = 'scorpio';
+            }else{
+                signd.value = 'sagittarius';
+            }
+            break;
+        case 12:
+            if(day < 22){
+                signd.value = 'sagittarius';
+            }else{
+                signd.value = 'capricorn';
+            }
+            break;
+    }
+}
